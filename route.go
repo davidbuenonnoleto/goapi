84a925ede25654d4ba483f1d7c9c078481f2ad44
David Noleto <dr.dave2@gmail.com>
2021-04-07 09:42:24 -0700

    add main files for API

---
+++ route.go
@@ -0,0 +1,8 @@
+package main
+
+type Route struct {
+	Id        string `json:"id,omitempty`
+	Driver    string `json:"driver,omitempty`
+	Zipcode   string `json:"zipcode,omitempty`
+	Numberpkg string `json:"numberpkg,omitempty`
+}
