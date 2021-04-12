164c249b16653f957f8b21d9aa6c474c727dee0a
David Noleto <dr.dave2@gmail.com>
2021-04-11 20:56:42 -0700

    add couchbase

--- route.go
+++ route.go
@@ -7,40 +7,41 @@ import (
 	"github.com/gorilla/context"
 	"github.com/gorilla/mux"
 	uuid "github.com/satori/go.uuid"
+	"gopkg.in/couchbase/gocb.v1"
 	"gopkg.in/go-playground/validator.v9"
 )
 
 type Route struct {
-	Id        string `json:"id,omitempty"`
-	Driver    string `json:"driver,omitempty"`
-	Zipcode   string `json:"zipcode,omitempty"`
-	Numberpkg string `json:"numberpkg,omitempty"`
-}
-
-var routes []Route = []Route{
-	{
-		Id:        "route-1",
-		Driver:    "driver-1",
-		Zipcode:   "94015",
-		Numberpkg: "15",
-	},
+	Id        string `json:"id,omitempty" validate:"omitempty,uuid"`
+	User      string `json:"user,omitempty" validate:"isdefault"`
+	Zipcode   string `json:"zipcode,omitempty" validate:"required"`
+	Numberpkg string `json:"numberpkg,omitempty" validate:"required"`
+	Type      string `json:"type,omitempty"`
 }
 
 func RouteRetrieveAllEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
+	var routes []Route
+	query := gocb.NewN1qlQuery(`SELECT ` + bucket.Name() + `.* FROM ` + bucket.Name() + ` WHERE type = 'route'`)
+	rows, _ := bucket.ExecuteN1qlQuery(query, nil)
+	var row Route
+	for rows.Next(&row) {
+		routes = append(routes, row)
+	}
 	json.NewEncoder(response).Encode(routes)
 }
 
 func RouteRetrieveEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	params := mux.Vars(request)
-	for _, route := range routes {
-		if route.Id == params["id"] {
-			json.NewEncoder(response).Encode(route)
-			return
-		}
+	var route Route
+	_, err := bucket.Get(params["id"], &route)
+	if err != nil {
+		response.WriteHeader(500)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
 	}
-	json.NewEncoder(response).Encode(Route{})
+	json.NewEncoder(response).Encode(route)
 }
 
 func RouteCreateEndpoint(response http.ResponseWriter, request *http.Request) {
@@ -56,23 +57,19 @@ func RouteCreateEndpoint(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	route.Id = uuid.Must(uuid.NewV4()).String()
-	route.Driver = token.Id
-	routes = append(routes, route)
-	json.NewEncoder(response).Encode(routes)
+	route.User = token.Id
+	route.Type = "article"
+	bucket.Insert(route.Id, route, 0)
+	json.NewEncoder(response).Encode(route)
 }
 
 func RouteDeleteEndpoint(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("content-type", "application/json")
 	params := mux.Vars(request)
 	token := context.Get(request, "decoded").(CustomJWTClaim)
-	for index, route := range routes {
-		if route.Id == params["id"] && route.Driver == token.Id {
-			routes = append(routes[:index], routes[index+1:]...)
-			json.NewEncoder(response).Encode(routes)
-			return
-		}
-	}
-	json.NewEncoder(response).Encode(Route{})
+	query := gocb.NewN1qlQuery(`DELETE FROM ` + bucket.Name() + ` WHERE id = $1 AND user = $2 AND type = 'route'`)
+	bucket.ExecuteN1qlQuery(query, []interface{}{params["id"], token.Id})
+	response.Write([]byte(`{ "message": "` + params["id"] + `" }`))
 }
 
 func RouteUpdateEndpoint(response http.ResponseWriter, request *http.Request) {
@@ -81,18 +78,26 @@ func RouteUpdateEndpoint(response http.ResponseWriter, request *http.Request) {
 	var changes Route
 	json.NewDecoder(request.Body).Decode(&changes)
 	token := context.Get(request, "decoded").(CustomJWTClaim)
-	for index, route := range routes {
-		if route.Id == params["id"] && route.Driver == token.Id {
-			if changes.Zipcode != "" {
-				route.Zipcode = changes.Zipcode
-			}
-			if changes.Numberpkg != "" {
-				route.Numberpkg = changes.Numberpkg
-			}
-			routes[index] = route
-			json.NewEncoder(response).Encode(routes)
-			return
-		}
+	queryStr := `UPDATE ` + bucket.Name() + ` SET type = 'route'`
+	if changes.Zipcode != "" {
+		queryStr += `, zipcode = $zipcode`
 	}
-	json.NewEncoder(response).Encode(Route{})
+	if changes.Numberpkg != "" {
+		queryStr += `, numberpkg = $numberpkg`
+	}
+	queryStr += ` WHERE id = $id AND user = $user AND type = 'route'`
+	query := gocb.NewN1qlQuery(queryStr)
+	queryParams := map[string]interface{}{
+		"zipcode":   changes.Zipcode,
+		"numberpkg": changes.Numberpkg,
+		"id":        params["id"],
+		"user":      token.Id,
+	}
+	_, err := bucket.ExecuteN1qlQuery(query, queryParams)
+	if err != nil {
+		response.WriteHeader(500)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	response.Write([]byte(`{ "message": "` + params["id"] + `" }`))
 }
